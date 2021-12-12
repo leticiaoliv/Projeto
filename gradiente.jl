2d0d61c7f7f1530ccf4f49c208f87f69669e92e7
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2021-12-12 17:03:10 -0300

    Update gradiente.jl

--- gradiente.jl
+++ gradiente.jl
@@ -1,9 +1,11 @@
-function gradiente(f::Function, grad::Array, z::Array)
+function gradiente(A::Matrix, G:: Matrix, y::Array, g::Array, z::Array)
     k = 0
     t = 1
     γ = 0.5
     η = 0.1
-    while abs(grad(z)) > 0
+    f(z) = (A*z - y).^2
+    grad(z) = G*z - g
+    while norm(grad(z)) > 0
         d = -grad(z)
         while f(z + t*d) > f(z) + η*t*grad(z)*d
             t = γ*t
