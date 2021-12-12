1f3e50f574aedf1d0b2aa7cdba37b6176d981a72
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2021-12-12 20:57:21 -0300

    Create newton.jl

---
+++ newton.jl
@@ -0,0 +1,18 @@
+function newton(A::Matrix, G:: Matrix, y::Array, g::Array, z::Array)
+    k = 0
+    t = 1
+    γ = 0.5
+    η = 0.1
+    f(z) = sum((A*z - y).^2)
+    grad(z) = G*z - g
+    hes = G
+    while norm(grad(z)) > 1e-6 && k < 1000000
+        d = -inv(hes))*grad(x)
+        while f(z + t*d) > f(z) + η*t*grad(z)*d
+            t = γ*t
+        end
+        z = z + t*d
+        k = k + 1
+    end
+    return z
+end
