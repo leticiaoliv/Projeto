f57383878dc7e21e2216a87ae029d72028ab30ec
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2021-12-13 07:32:34 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -5,10 +5,72 @@ using DelimitedFiles
 function main()
     x = readdlm("dados_x.csv");
     y = readdlm("dados_y.csv");
+
+    A = [ones(52) x];
+    G = [52 sum(x); 
+        sum(x) sum(x.^2)];
+    g = [sum(y); sum(x.*y)];
+    z = ones(2);
+
+    gr = gradiente(A, G, y, g, z)
+    nt = newton(A, G, y, g, z)
+    errograd = norm(gr)/norm(y);
+    erronewt = norm(nt)/norm(y);
+    print("Aproximação Linear")
+    print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
+    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+
+    scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
+    plot!(x -> gr[1] + gr[2]*x, label = "Gradiente", c=:red, lw=3)
+    plot!(x -> nt[1] + nt[2]*x, label = "Newton", c=:green, lw=3)
+    xlabel!("Semanas")
+    ylabel!("Número de Casos")
+    png("AproxLin")
+
     A = [ones(52) x x.^2];
-    G = [52 sum(x) sum(x.^2); sum(x) sum(x.^2) sum(x.^3); sum(x.^2) sum(x.^3) sum(x.^4)];
+    G = [52 sum(x) sum(x.^2); 
+        sum(x) sum(x.^2) sum(x.^3); 
+        sum(x.^2) sum(x.^3) sum(x.^4)];
     g = [sum(y); sum(x.*y); sum(x.^2 .*y)];
     z = ones(3);
 
-    z = gradiente(A, G, y, g, z)
+    gr = gradiente(A, G, y, g, z)
+    nt = newton(A, G, y, g, z)
+    errograd = norm(gr)/norm(y);
+    erronewt = norm(nt)/norm(y);
+    print(" Aproximação Quadrática")
+    print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
+    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+
+    scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
+    plot!(x -> gr[1] + gr[2]*x + gr[3]*x.^2, label = "Gradiente", c=:red, lw=3)
+    plot!(x -> nt[1] + nt[2]*x + nt[3]*x.^2, label = "Newton", c=:green, lw=3)
+    xlabel!("Semanas")
+    ylabel!("Número de Casos")
+    png("AproxQuad")
+
+    A = [ones(52) x x.^2 x.^3];
+    G = [52 sum(x) sum(x.^2) sum(x.^3);
+        sum(x) sum(x.^2) sum(x.^3) sum(x.^4);
+        sum(x.^2) sum(x.^3) sum(x.^4) sum(x.^5);
+        sum(x.^3) sum(x.^4) sum(x.^5) sum(x.^6)];
+    g = [sum(y); sum(x.*y); sum(x.^2 .*y); sum(x.^3 .*y)];
+    z = ones(4);
+
+    gr = gradiente(A, G, y, g, z)
+    nt = newton(A, G, y, g, z)
+    errograd = norm(gr)/norm(y);
+    erronewt = norm(nt)/norm(y);
+    print(" Aproximação Cúbica")
+    print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
+    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+
+
+    scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
+    plot!(x -> gr[1] + gr[2]*x + gr[3]*x.^2 + gr[4]*x.^3, label = "Gradiente", c=:red, lw=3)
+    plot!(x -> nt[1] + nt[2]*x + nt[3]*x.^2 + nt[4]*x.^3, label = "Newton", c=:green, lw=3)
+    xlabel!("Semanas")
+    ylabel!("Número de Casos")
+    png("AproxCub")
+
 end
