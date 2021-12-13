0edca148ee88d2943a7bcb87a19a6b135c4ea2b6
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2021-12-13 19:20:04 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -7,18 +7,20 @@ function main()
     y = readdlm("dados_y.csv");
 
     A = [ones(52) x];
-    G = [52 sum(x); 
-        sum(x) sum(x.^2)];
+    G = [52 sum(x); sum(x) sum(x.^2)];
     g = [sum(y); sum(x.*y)];
     z = ones(2);
 
     gr = gradiente(A, G, y, g, z)
     nt = newton(A, G, y, g, z)
-    errograd = norm(gr)/norm(y);
-    erronewt = norm(nt)/norm(y);
-    print("Aproximação Linear")
+    errograd = sqrt(sum((A*gr - y).^2))/norm(y);
+    erronewt = sqrt(sum((A*nt - y).^2))/norm(y);
+    print(" Aproximação Linear")
     print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
-    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+    print(" Erro Absoluto Gradiente = ", sqrt(sum((A*gr - y).^2)),
+    " Erro Absoluto Newton =", sqrt(sum((A*nt - y).^2)))
+    print(" Erro Relativo Gradiente = ", errograd, " Erro Relativo Newton = ", erronewt)
+
 
     scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
     plot!(x -> gr[1] + gr[2]*x, label = "Gradiente", c=:red, lw=3)
@@ -28,19 +30,19 @@ function main()
     png("AproxLin")
 
     A = [ones(52) x x.^2];
-    G = [52 sum(x) sum(x.^2); 
-        sum(x) sum(x.^2) sum(x.^3); 
-        sum(x.^2) sum(x.^3) sum(x.^4)];
+    G = [52 sum(x) sum(x.^2); sum(x) sum(x.^2) sum(x.^3); sum(x.^2) sum(x.^3) sum(x.^4)];
     g = [sum(y); sum(x.*y); sum(x.^2 .*y)];
     z = ones(3);
 
     gr = gradiente(A, G, y, g, z)
     nt = newton(A, G, y, g, z)
-    errograd = norm(gr)/norm(y);
-    erronewt = norm(nt)/norm(y);
+    errograd = sqrt(sum((A*gr - y).^2))/norm(y);
+    erronewt = sqrt(sum((A*nt - y).^2))/norm(y);
     print(" Aproximação Quadrática")
     print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
-    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+    print(" Erro Absoluto Gradiente = ", sqrt(sum((A*gr - y).^2)),
+    " Erro Absoluto Newton =", sqrt(sum((A*nt - y).^2)))
+    print(" Erro Relativo Gradiente = ", errograd, " Erro Relativo Newton = ", erronewt)
 
     scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
     plot!(x -> gr[1] + gr[2]*x + gr[3]*x.^2, label = "Gradiente", c=:red, lw=3)
@@ -59,11 +61,13 @@ function main()
 
     gr = gradiente(A, G, y, g, z)
     nt = newton(A, G, y, g, z)
-    errograd = norm(gr)/norm(y);
-    erronewt = norm(nt)/norm(y);
+    errograd = sqrt(sum((A*gr - y).^2))/norm(y);
+    erronewt = sqrt(sum((A*nt - y).^2))/norm(y);
     print(" Aproximação Cúbica")
     print(" Coeficientes Gradiente = ", gr, " Coeficientes Newton = ", nt)
-    print(" Erro Absoluto Gradiente = ", errograd, " Erro Absoluto Newton = ", erronewt)
+    print(" Erro Absoluto Gradiente = ", sqrt(sum((A*gr - y).^2)),
+    " Erro Absoluto Newton =", sqrt(sum((A*nt - y).^2)))
+    print(" Erro Relativo Gradiente = ", errograd, " Erro Relativo Newton = ", erronewt)
 
 
     scatter(x, y, title = "Contaminados COVID-19",label = "Casos por Semana", lw = 3)
