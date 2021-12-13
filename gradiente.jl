0f0c76649555210037d3311d309390f4943a6940
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2021-12-13 07:34:29 -0300

    Update gradiente.jl

--- gradiente.jl
+++ gradiente.jl
@@ -5,7 +5,7 @@ function gradiente(A::Matrix, G:: Matrix, y::Array, g::Array, z::Array)
     η = 0.1
     f(z) = sum((A*z - y).^2)
     grad(z) = G*z - g
-    while norm(grad(z)) > 1e-6 || k < 100
+    while norm(grad(z)) > 1e-6 && k < 10000
         d = -grad(z)
         while f(z + t*d) > f(z) + η*t*grad(z)'*d
             t = γ*t
